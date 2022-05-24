5fa6f75db2fda7c4f36d809d7a66f2c38271c94f
Дмитрий Брилько <DimaBrilko@gmail.com>
2022-05-24 18:22:04 +0300

    проверка

--- SecureEVotingSystem/MainWindow.xaml.cs
+++ SecureEVotingSystem/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace SecureEVotingSystem {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();//
+            //
         }
     }
 }
